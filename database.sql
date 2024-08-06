fac4d1c1534e955cd595374f1faf625987c7206c
[Akorsikov] <[akors@mail.ru]>
2024-08-06 14:34:19 +0300

    feat: database.aql

--- database.sql
+++ database.sql
@@ -8,7 +8,13 @@ DROP TABLE IF EXISTS urls CASCADE;
 
 -- Создать таблицу 'urls'
 CREATE TABLE urls (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    created_at TIMESTAMP
+    created_at TIMESTAMP(0) DEFAULT NOW()
 );
+
+-- 
+GRANT ALL PRIVILEGES ON urls TO aleksandr;
+
+--
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO aleksandr;
