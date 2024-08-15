92f4177918dd70c904b08de3529113a1d4f77ac5
[Akorsikov] <[akors@mail.ru]>
2024-08-15 22:39:14 +0300

    refactor: simplified

--- database.sql
+++ database.sql
@@ -1,14 +1,3 @@
-
--- -- Создать БД 'websites_db';
--- DO $$
--- BEGIN
---     IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'websites_db_trjx') THEN
---         EXECUTE 'CREATE DATABASE websites_db';
---     ENDIF;
--- END $$;
-
--- CREATE USER aleksandr WITH ENCRYPTED PASSWORD '123456';
-
 -- Удалить таблицы 'urls, url_checks' если существют
 DROP TABLE IF EXISTS urls, url_checks CASCADE;
 
@@ -29,9 +18,3 @@ CREATE TABLE url_checks (
     description TEXT,
     created_at TIMESTAMP(0) DEFAULT NOW()
 );
-
--- 
-GRANT ALL PRIVILEGES ON urls, url_checks TO aleksandr;
-
---
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO aleksandr;
