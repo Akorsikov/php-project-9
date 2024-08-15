eafdc88edfd5f5216c1f79ebb89f533be85a0f91
[Akorsikov] <[akors@mail.ru]>
2024-08-15 09:27:05 +0300

    feat: url_cheks(url_id) REFERENCES urls(id)

--- database.sql
+++ database.sql
@@ -15,17 +15,17 @@ DROP TABLE IF EXISTS urls, url_checks CASCADE;
 -- Создать таблицу 'urls'
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    name varchar(255),
     created_at TIMESTAMP(0) DEFAULT NOW()
 );
 
 -- Создать таблицу 'url_checks'
 CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY,
-    url_id INTEGER NOT NULL,
-    status_code INТ NOT NULL,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
+    url_id integer REFERENCES urls(id) NOT NULL,
+    status_code integer,
+    h1 varchar(255),
+    title varchar(255),
     description TEXT,
     created_at TIMESTAMP(0) DEFAULT NOW()
 );
